4c780c469ad4e93a7ec777ae6d23d1b9d208e3d8
Evgeniy Fedorin <revike@ya.ru>
2022-09-11 11:30:31 +0300

    fix Brush.js

--- my-app/src/tools/Brush.js
+++ my-app/src/tools/Brush.js
@@ -1,165 +1,87 @@
 import Tool from "./Tool";
 
-
-let coords = { /*Отсюда вызываются данные переданные с сервера на клиент и пишутся данные с клиента на сервер(после передачи на се
-    рвер и после отрисовки данных с сервера, данные в этом объекте стираются для записи данных о новом объекте)*/
-            
-    "coord": [],
-    "type": "r",
-    "color": null,
-    "width": null,
-    "other_data": null
-};
-
-
 /* ОБРАЩЕНИЕ К СЕРВЕРУ ДЛЯ ПОЛУЧЕНИЯ ДАННЫХ ЧЕРЕЗ СОКЕТ */
 
-let socket = new WebSocket("ws://localhost:8000/board/2/");
-
-/* ТЕСТОВЫЕ ДАННЫЕ */
-
-// let addObj = {"type": "ADD_OBJECT", 
-// "data": {"object": {"type": "r", 
-// "points": "[502,213],[504,213],[506,213],[510,213],[513,214],[514,214],[515,214],[517,215],[518,215],[519,215],[520,215],[521,215],[522,215],[523,215],[524,216],[525,216],[526,216]", "id": "27"}}}
-
-// let initData = {"type": "INITIAL_DATA", "data": {"objects": [{"type": "r", "points": "[487,400],[488,400],[489,400],[491,401],[493,401],[494,401],[495,401],[497,401],[498,402],[498,402],[499,402],[500,403],[501,403],[502,403],[502,403],[503,403]",
-//  "id": "26"}, {"type": "r", "points": "[482,609],[483,609],[483,609],[485,609],[486,609],[486,609],[487,610],[489,610],[490,610],[490,610],[491,610],[492,610],[493,610],[493,611],[494,611],[494,611],[494,611],[495,611],[496,611],[497,611]", "id": "26"}]}}
-
-
-
-/* ОБРАЩЕНИЕ К СЕРВЕРУ ДЛЯ ПОЛУЧЕНИЯ ДАННЫХ ЧЕРЕЗ HTTP */
-
-// const getData = async (url) => {
-
-//     const response = await fetch(url);
-  
-//     if (!response.ok) {
-//       throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
-//     }
-  
-//     return await response.json();
-  
-//   };
-
-//   let get_server =  getData('https://jsonplaceholder.typicode.com/todos/1'); //тестовые данные со стороннего сервера 
-//   get_server.then(data => coords.coord.push([[data.userId + 200, data.userId + 200], [248, 471],
-//     [256, 464], [278, 446], [350, 402], [425, 370], [493, 356], [517, 356], [560, 363], [577, 374], [582, 378]], [[590, 389],
-//     [595, 396], [599, 400], [600, 401], [606, 403], [613, 402], [693, 356], [751, 318], [848, 255], [890, 226],[893, 222]])); 
-//     // эти данные придут c сервера в формате {"coord": [[x, y], [x, y]], [[x, y], [x, y]]} 
-
+let socket = new WebSocket("ws://localhost:8000/board/1/");
 
 export default class Brush extends Tool {
+    object;
+    objects;
+
     constructor(canvas) {
         super(canvas);
         this.listen();
-        this.count = 0; //Кол-во кликов для определения количества объектов создынных на холсте
-//        this.socket.onmessage = function(e){
-//            var data = JSON.parse(e.data)
-//            for (let object of data.data.objects) {
-//                    coords.coord.push(object.coord);
-//                    drawLine()
-//            }
-//        }
-
+        this.count = 0;
+        this.coords = {
+            "coord": [],
+            "type": "r",
+            "color": null,
+            "width": null,
+            "other_data": null
+        };
     };
-
 
     listen() {
         this.canvas.onmousemove = this.mouseMoveHandler.bind(this)
         this.canvas.onmousedown = this.mouseDownHandler.bind(this)
         this.canvas.onmouseup = this.mouseUpHandler.bind(this)
         socket.onmessage = (e) => {
-            var data = JSON.parse(e.data)
-            for (let object of data.data.objects) {
-                    coords.coord.push(object.coord);
-                    this.drawLine()
+            let data = JSON.parse(e.data)
+            let data_objects = data.data.objects;
+
+            for (let object of data_objects) {
+                this.coords.coord.push(object.coord);
+                this.drawLine(this.coords.coord)
             }
+            this.coords.coord = []
         }
     };
 
-
-     drawLine() {
-        for (let coord_array of coords.coord) {
-                this.ctx.beginPath();
-                this.ctx.moveTo(coord_array[0][0],coord_array[0][1]);
-                for (let arr in coord_array) {
-                    this.ctx.lineTo(`${coord_array[arr][0]}`,`${coord_array[arr][1]}`);
-                    this.ctx.stroke();
-                }
+    drawLine(coord) {
+        for (let coord_array of coord) {
+            this.ctx.beginPath();
+            this.ctx.moveTo(coord_array[0][0], coord_array[0][1]);
+            for (let arr in coord_array) {
+                this.ctx.lineTo(`${coord_array[arr][0]}`, `${coord_array[arr][1]}`);
+                this.ctx.stroke();
             }
+        }
         this.count++
-        coords.coord = [];
+    };
 
-     };
-
-    mouseUpHandler(e) {
-        
+    mouseUpHandler() {
         this.mouseDown = false;
 
-        if(!this.mouseDown) {
-            for (let coord_array of coords.coord) {
-                this.ctx.beginPath();
-                this.ctx.moveTo(coord_array[0][0],coord_array[0][1]);
-                for (let arr in coord_array) {
-                    this.ctx.lineTo(`${coord_array[arr][0]}`,`${coord_array[arr][1]}`);
-                    this.ctx.stroke();
-                }
+        if (!this.mouseDown) {
+            this.drawLine(this.coords.coord)
+            this.count++
+
+            if (this.coords.coord.length > 0) {
+                /* ПЕРЕДАЧА ДАННЫХ СЕРВЕРУ ОТ КЛИЕНТА ЧЕРЕЗ СОКЕТЫ */
+                socket.send(JSON.stringify(this.coords));
+
+                socket.onclose = function (e) {
+                    if (e.wasClean) {
+                        console.log(`[close] Соединение закрыто чисто, код=${e.code} причина=${e.reason}`);
+                    } else {
+                        console.log('[close] Соединение прервано');
+                    }
+                };
+
+                socket.onerror = function (error) {
+                    console.log(`[error] ${error.message}`);
+                };
+
+                this.coords.coord = [];
+
             }
-
-        this.count++
-
-
-        if (coords.coord.length > 0) {
-            // ПЕРЕДАЧА ДАННЫХ СЕРВЕРУ ОТ КЛИЕНТА ЧЕРЕЗ http
-            // const sendData = async (url, data) => {
-            //     const response = await fetch(url, {
-            //       method: 'POST',
-            //       body: data,
-            //     });
-              
-            //     if (!response.ok) {
-            //       throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
-            //     }
-              
-            //     return await response.json();
-            //   }
-            
-            //   sendData("#", JSON.stringify(coords));
-            
-
-            /* ПЕРЕДАЧА ДАННЫХ СЕРВЕРУ ОТ КЛИЕНТА ЧЕРЕЗ СОКЕТЫ */
-            console.log(coords.coord)
-            socket.send(JSON.stringify(coords));
-
-            socket.onclose = function (e) {
-                if (e.wasClean) {
-                  console.log(`[close] Соединение закрыто чисто, код=${e.code} причина=${e.reason}`);
-                } else {
-                    console.log('[close] Соединение прервано');
-                }
-              };
-              
-            socket.onerror = function (error) {
-                console.log(`[error] ${error.message}`);
-            };
-
-      //То, что клиент отправит серверу(тестовый вывод в консоль, пока нет url)
-            coords.coord = []; 
-
         }
-    }
 
     };
 
     mouseDownHandler(e) {
 
-        if (coords.coord.length > 0) {
-            this.mouseDown = false;
-        }
-        else {
-
-            this.mouseDown = true;
-        }
+        this.mouseDown = this.coords.coord.length <= 0;
 
         this.ctx.beginPath();
         this.ctx.moveTo(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop);
@@ -176,7 +98,7 @@ export default class Brush extends Tool {
     draw(x, y) {
 
         if (this.mouseDown) {
-            coords.coord.push([x, y]);
+            this.coords.coord.push([x, y]);
             this.ctx.lineTo(x, y);
             this.ctx.stroke();
         }
