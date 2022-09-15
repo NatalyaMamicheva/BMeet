8b7bcb677813bae87e63cfd6b4e33589ecc5d0e4
e.murij <murij.kat@gmail.com>
2022-09-15 21:36:20 +0700

    add verification link processing

---
+++ my-app/src/components/BoardManagement.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class BoardManagement extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+             <div>
+                  <h1>Доски</h1>
+             </div>
+        )
+    }
+}
+
+export default BoardManagement
