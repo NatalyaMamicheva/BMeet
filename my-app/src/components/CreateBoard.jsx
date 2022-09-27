3a922492ca1f9240705330f49d9c97eea2ad6d00
Evgeniy Fedorin <revike@ya.ru>
2022-09-27 16:05:45 +0300

    style model window

--- my-app/src/components/CreateBoard.jsx
+++ my-app/src/components/CreateBoard.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import axios from 'axios'
-import Header from './Header.jsx'
-import {Navigate, Link} from 'react-router-dom'
 
 
 class CreateBoard extends React.Component {
     constructor(props) {
         super(props)
-         this.state = {
-            'name':'',
-            'description':'',
-            'group':'',
-            'error_message':''
-         };
-         this.errorRef = React.createRef();
+        this.state = {
+            'name': '',
+            'description': '',
+            'group': '',
+            'error_message': ''
+        };
+        this.errorRef = React.createRef();
     }
 
     handleCreateSubmit(event) {
@@ -34,14 +32,14 @@ class CreateBoard extends React.Component {
             headers: headers,
             data: data
         })
-        .then(response => {
-            this.setState({'error_message': '' });
-            this.props.handleShowCreateBoard(event)
-        })
-        .catch(error => {
-            if (error.code == 'ERR_BAD_REQUEST') this.setState({'error_message': 'Проверьте правильность введеных email' });
-            else this.setState({'error_message': error.message });
-        })
+            .then(response => {
+                this.setState({'error_message': ''});
+                this.props.handleShowCreateBoard(event)
+            })
+            .catch(error => {
+                if (error.code == 'ERR_BAD_REQUEST') this.setState({'error_message': 'Проверьте правильность введеных email'});
+                else this.setState({'error_message': error.message});
+            })
         event.preventDefault()
     }
 
@@ -53,20 +51,76 @@ class CreateBoard extends React.Component {
 
     render() {
         return (
-           <form onSubmit={(event) => this.handleCreateSubmit(event)} >
-              <p className='error_p' ref={this.errorRef} >{this.state.error_message}</p>
-              <button onClick={(event) =>  this.props.handleShowCreateBoard(event)}> X </button>
-              <h3>Название доски</h3>
-              <input name="name" type="text" required
-                    onChange={(event) => this.handleChange(event)} value={this.state.name}></input>
-              <h3>Описание</h3>
-              <textarea name="description"
-                    onChange={(event) => this.handleChange(event)} value={this.state.description}></textarea>
-              <h3>Пригласить коллег</h3>
-              <input name="group" type="text"
-                    onChange={(event) => this.handleChange(event)} value={this.state.group}></input>
-              <button type="submit" value="submit"> Сохранить </button>
-          </form>
+            <div className='new_board'>
+                <form className='new_board_form'
+                      onSubmit={(event) => this.handleCreateSubmit(event)}>
+                    <p ref={this.errorRef}>{this.state.error_message}</p>
+                    <div className="new_board_close">
+                        <div
+                            onClick={(event) => this.props.handleShowCreateBoard(event)}>
+                            <div className="new_board_close_button"></div>
+                        </div>
+                    </div>
+                    <div className="new_board_title">
+                        <p>Создание доски</p>
+                    </div>
+                    <div className="new_board_text">
+                        <p>Создайте новую доску в BMeet и предложите своим
+                            коллегам присоединиться к Вам</p>
+                    </div>
+
+                    <div className="new_board_input">
+                        <div className="new_board_input_title">
+                            <span>Название</span>
+                        </div>
+                        <div className="new_board_input_border">
+                            <label>
+                                <input className='new_board_input_text'
+                                       name="name" type="text" required
+                                       placeholder='Введите название доски'
+                                       onChange={(event) => this.handleChange(event)}
+                                       value={this.state.name}></input>
+                            </label>
+                        </div>
+
+                        <div className="new_board_input_title">
+                            <span>Описание</span>
+                        </div>
+                        <div className="new_board_input_border">
+                            <label>
+                                <textarea name="description"
+                                          placeholder='Введите описание'
+                                          className='new_board_input_text new_board_input_text_area'
+                                          onChange={(event) => this.handleChange(event)}
+                                          value={this.state.description}>
+                                </textarea>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="new_board_invite_title">
+                        <p>Пригласить коллег</p>
+                    </div>
+                    <div className="new_board_invite_text">
+                        <p>
+                            Введите адреса электронной почты ваших коллег и
+                            пригласите их присоединиться к вашей доске в
+                            BMeet
+                        </p>
+                        <div className="new_board_input_border">
+                            <label>
+                                <input className='new_board_input_text'
+                                       name="group" type="email"
+                                       onChange={(event) => this.handleChange(event)}
+                                       value={this.state.group}></input>
+                            </label>
+                        </div>
+                        <div className="new_board_invite_button">
+                            <button type="submit" value="submit">Создать
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
