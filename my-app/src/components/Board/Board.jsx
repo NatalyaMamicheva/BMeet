1a1f3207725b990321a37642f3211cab03345e83
e.murij <murij.kat@gmail.com>
2022-10-13 17:39:47 +0700

    add board name to board

--- my-app/src/components/Board/Board.jsx
+++ my-app/src/components/Board/Board.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import '../../styles/board.scss';
+import Header from '../Header.jsx'
 import Toolbar from "./Toolbar";
 import Canvas from "./Canvas";
 
 const Board = () => {
 
+    const params = new URLSearchParams(window.location.search);
+    const name = params.get('name');
+    console.log(name)
+
+
     return (
         <div>
+            <Header board_name={name} />
             <Toolbar />
             <Canvas />
         </div>
